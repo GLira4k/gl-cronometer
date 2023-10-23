df619f9c14eb22d781ef98e6b622e40b7fc1a22a
GLira4k <103190663+GLira4k@users.noreply.github.com>
2023-10-23 09:27:58 -0300

    Update
    Finalizei o projeto do cronômetro, vou fazer alguns ajustes visuais para deixá-lo melhor.

--- js/index.js
+++ js/index.js
@@ -14,7 +14,11 @@ let seconds = 0;
 let milliseconds = 0;
 let isPaused = false;
 
-startBtn.addEventListener("click", startTimer)
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
+
 
 function startTimer(){
 
@@ -34,12 +38,53 @@ function startTimer(){
                 seconds = 0;
             };
 
-            minutesEl.textContent = minutes;
-            secondsEl.textContent = seconds;
-            millisecondsEl.textContent = milliseconds;
+            minutesEl.textContent = formatTime(minutes);
+            secondsEl.textContent = formatTime(seconds);
+            millisecondsEl.textContent = formatMilliseconds(milliseconds);
         
         }
     }, 10);
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+}
+
+function resetTimer(){
+    clearInterval(interval);
+
+    isPaused = false;
+
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "00";
+
+    resumeBtn.style.display = "none";
+    pauseBtn.style.display = "none";
+    startBtn.style.display = "block";
+}
+
+function resumeTimer(){
+    isPaused = false;
+    resumeBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+}
+
+function pauseTimer(){
+    isPaused = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time){
+    return time < 100 ? `${time}`.padStart(3, "0") : time;
 }
 
 });
